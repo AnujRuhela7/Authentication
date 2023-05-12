948c5790b12fcd5068e5f0ec8fa9576e1a445b47
AnujRuhela7 <ruhela7777@gmail.com>
2023-05-12 11:12:51 +0530

    Used MD5 Hashing to secure passwords

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,10 +24,11 @@ mongoose
   .catch((err) => console.log(err));
 
 // User Modal
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+const secret = process.env.SECRET_KEY;
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -43,7 +46,7 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   await newUser.save();
   res.render("secrets");
@@ -51,7 +54,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   const user = await User.findOne({ email: username });
 
--- auth.md
+++ auth.md
@@ -1,7 +1,33 @@
-## Level 1 Authentication
+### Level 1 Authentication
 
 ## Using username & Password
 
 # Just checking the username in the db, and if it exist check the password
 
 # If the password matches, redirect tp secrets page, else if it doesn't redirect to login page, stating that "Password is incorrect"
+
+### Level 2 Authentication
+
+# Encrypt the password
+
+# Caesar Cipher is one of the earliest method to encryot a message
+
+## Ceaser Cipher is a very weak method for encryption
+
+# Just shift the letters or number by some n position forward or backward
+
+# mongoose-encryption package is used here to encrypt the password
+
+# This package can be used to encrypt and auhenticate
+
+# it uses AES-256-CBC encryption algorithm
+
+# userSchema.plugin(encrypt, { secret: secret, encryptrdFields: ["password"] });
+
+# with the encryptedFields we can specify the custom fields which we only want to encrypt
+
+# the documents are encrypted on saving
+
+# and decrypted when we call find
+
+## Level 3 Hashing

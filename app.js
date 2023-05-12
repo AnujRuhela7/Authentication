0da75c5342a29690aff107a28b1a8eb688098e3d
AnujRuhela7 <ruhela7777@gmail.com>
2023-05-12 10:03:57 +0530

    Level 1 Authentication

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose
+  .connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log(err));
+
+// User Modal
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  await newUser.save();
+  res.render("secrets");
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.findOne({ email: username });
+
+  if (user) {
+    if (user.password === password) {
+      res.render("secrets");
+    } else {
+      console.log("Wrong Password");
+      res.render("login");
+    }
+  } else {
+    console.log("User Not Found");
+    res.render("login");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is up and running at port 3000");
+});
---
+++ auth.md
@@ -0,0 +1,7 @@
+## Level 1 Authentication
+
+## Using username & Password
+
+# Just checking the username in the db, and if it exist check the password
+
+# If the password matches, redirect tp secrets page, else if it doesn't redirect to login page, stating that "Password is incorrect"
